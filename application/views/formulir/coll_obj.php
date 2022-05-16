dd3073542070c4a9f9ae8a21e4414df8d7af4111
duwilaja <apaaja.software@gmail.com>
2022-05-16 15:55:16 +0700

    coll obj rawan emerg

---
+++ application/views/formulir/coll_obj.php
@@ -0,0 +1,121 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); 
+
+$cols="nrp,unit,polda,polres,dinas,subdinas,tgl,";
+$cols.="jam,detil,nama,lat,lng";
+?>
+<input type="hidden" name="path" value="coll_obj">
+
+<input type="hidden" name="tablename" value="coll_obj">
+<input type="hidden" name="fieldnames" value="<?php echo $cols?>">
+
+<!--input type="hidden" name="tgl" value="<?php echo date('Y-m-d')?>"-->
+
+<div class="row">
+	<div class="col-sm-6 col-md-3">
+		<div class="form-group">
+			<label class="form-label">Tanggal</label>
+			<input readonly type="text" id="tgl" name="tgl" class="form-control" placeholder="[auto]" value="<?php echo date('Y-m-d')?>" >
+		</div>
+	</div>
+	<div class="col-sm-6 col-md-2">
+		<div class="form-group">
+			<label class="form-label">Jam</label>
+			<input readonly type="text" id="jam" name="jam" class="form-control" placeholder="[auto]" value="<?php echo date('H:i:s')?>" >
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-sm-6 col-md-4">
+		<div class="form-group">
+			<label class="form-label">Nama</label>
+			<input type="text" id="nama" name="nama" class="form-control" placeholder="" >
+		</div>
+	</div>
+	<div class="col-sm-6 col-md-2">
+		<div class="form-group">
+			<label class="form-label">Latitude</label>
+			<input type="text" id="lat" name="lat" class="form-control" placeholder="" >
+		</div>
+	</div>
+	<div class="col-sm-6 col-md-2">
+		<div class="form-group">
+			<label class="form-label">Longitude</label>
+			<input type="text" id="lng" name="lng" class="form-control" placeholder="" >
+		</div>
+	</div>
+	<div class="col-sm-6 col-md-1">
+		<div class="form-group">
+			<label class="form-label">&nbsp;</label>
+			<button type="button" class="btn btn-danger" onclick="mappicker('#lat','#lng');"><i class="fa fa-map-marker"></i></button>
+		</div>
+	</div>
+	<div class="col-sm-6 col-md-4">
+		<div class="form-group">
+			<label class="form-label">Detil</label>
+			<textarea id="detil" name="detil" class="form-control" placeholder="" ></textarea>
+		</div>
+	</div>
+	
+</div>
+<!--div class="row">
+	<div class="col-sm-6 col-md-5">
+		<div class="form-group">
+			<label class="form-label">Uraian</label>
+			<textarea id="uraian" name="uraian" class="form-control" placeholder="" ></textarea>
+		</div>
+	</div>
+	<div class="col-sm-6 col-md-4">
+		<div class="form-group files">
+			<label class="form-label">Foto/Video</label>
+			<input type="file" name="uploadedfile[]" class="form-control file" placeholder="" >
+		</div>
+	</div>
+	<div class="col-sm-6 col-md-1">
+		<label class="form-label">&nbsp;</label>
+		<button type="button" class="btn btn-icon btn-facebook" onclick="$('.files').append($('.file').clone().removeClass('file'));"><i class="fa fa-copy"></i></button>
+	</div>
+</div-->
+
+
+<script>
+function mappicker(lat,lng){
+	window.open(base_url+"map?lat="+$(lat).val()+"&lng="+$(lng).val(),"MapWindow","width=830,height=500,location=no").focus();
+}
+function jenischanged(tv){
+	if(tv=='Yan Aduan'){
+		$(".aduan").show();
+	}else{
+		$("#jenisd").val("");
+		$(".aduan").hide();
+	}
+}
+jvalidate = $("#myf").validate({
+    rules :{
+        "formulir" : {
+            required : true
+        },
+		"dasar" : {
+            required : true
+        },
+		"nomor" : {
+			required : true
+		},
+		"lat" : {
+			required : true
+		},
+		"lng" : {
+			required : true
+		}
+    }});
+
+$("#btn_save").show();
+$(".dasar").show();
+$(".nomor").show();
+
+datepicker(true);
+timepicker();
+
+$(".is-invalid").removeClass("is-invalid");
+$(".is-valid").removeClass("is-valid");
+
+</script>
---
+++ application/views/rekap/coll_obj.php
@@ -0,0 +1,66 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); 
+
+$cols="nrp,unit,polda,polres,dinas,subdinas,";
+$cols="'' as btnset,nrp,tgl,jam,nama,lat,lng,detil,rowid";
+$tname="coll_obj";
+?>
+
+<?php $this->load->view('rekap/incl/head_rekap');?>
+		<div class="table-responsive mt-4">
+			<table id="mytbl" class="table table-striped table-bordered w-100">
+				<thead>
+					<tr>
+						<th></th>
+						<th>ID/NRP</th>
+						<th>Tanggal</th>
+						<th>Jam</th>
+						<th>Lokasi</th>
+						<th>Latitude</th>
+						<th>Longitude</th>
+						<th>Detil</th>
+					</tr>
+				</thead>
+				<tbody>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+
+<script>
+var  mytbl;
+function load_table(){
+	mytbl = $("#mytbl").DataTable({
+		serverSide: true,
+		processing: true,
+		searching: false,
+		ordering:false,
+		buttons: ['copy', {extend : 'excelHtml5', messageTop: $(".judul").text()}],
+		ajax: {
+			type: 'POST',
+			url: '<?php echo base_url()?>rekap/datatable',
+			data: function (d) {
+				d.cols= '<?php echo base64_encode($cols); ?>',
+				d.tname= '<?php echo base64_encode($tname); ?>',
+				d.orders= '<?php echo base64_encode("tgl desc, jam desc, rowid desc"); ?>',
+				d.ismap= true,
+				d.isedit= true,
+				d.fdate= $('#fdate').val(),
+				d.tdate= $('#tdate').val();
+			}
+		},
+		initComplete: function(){
+			dttbl_buttons(); //for ajax call
+		}
+	});
+	datepicker();
+}
+
+function contentcallback(){
+	load_table();
+}
+
+function reload_table(){
+	mytbl.ajax.reload();
+}
+</script>
