aa4ec1570afdcb5f2fd35967d346e2a985996c49
yusfamb07 <yusfamuhammadbakran@gmail.com>
2022-05-19 17:48:02 +0700

    icon

--- application/views/templates/cuba.php
+++ application/views/templates/cuba.php
@@ -20,8 +20,8 @@ if (count($farr) > 0 && $session['nrp'] != '') {
     <meta name="description" content="Cuba admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 admin template with unlimited possibilities.">
     <meta name="keywords" content="admin template, Cuba admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="pixelstrap">
-    <link rel="icon" href="<?php echo $base_url; ?>cuba/assets/images/favicon.png" type="image/x-icon">
-    <link rel="shortcut icon" href="<?php echo $base_url; ?>cuba/assets/images/favicon.png" type="image/x-icon">
+    <link rel="icon" href="<?php echo $base_url; ?>cuba/assets/images/logo-army2.png" type="image/x-icon">
+    <link rel="shortcut icon" href="<?php echo $base_url; ?>cuba/assets/images/logo-army2.png" type="image/x-icon">
     <title>Smart Teritory</title>
     <!-- Google font-->
     <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap" rel="stylesheet">
